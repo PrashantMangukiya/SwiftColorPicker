33f57aca6a1c36c0c100d147ac435ea312a3ff95
Michael Sheaver <msheaver@me.com>
2016-09-05 22:20:44 -0400

    Updated for Swift 3, iOS 10, Xcode 8

--- SwiftColorPicker/SwiftColorPicker/ColorPickerViewController.swift
+++ SwiftColorPicker/SwiftColorPicker/ColorPickerViewController.swift
@@ -13,7 +13,7 @@ import UIKit
 protocol ColorPickerDelegate {
     
     // return selected color as UIColor and Hex string
-    func colorPickerDidColorSelected(selectedUIColor selectedUIColor: UIColor, selectedHexColor: String)
+    func colorPickerDidColorSelected(selectedUIColor: UIColor, selectedHexColor: String)
 }
 
 
@@ -71,25 +71,25 @@ class ColorPickerViewController: UIViewController, UICollectionViewDataSource, U
     // MARK: - Collection view Datasource & Delegate functions
     
     // return number of section in collection view
-    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
     }
     
     
     // return number of cell shown within collection view
-    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return self.colorList.count
     }
     
     
     // create collection view cell content
-    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
         // deque reusable cell from collection view.
-        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("ColorCell", forIndexPath: indexPath) 
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ColorCell", for: indexPath) 
         
         // set cell background color from given color list
-        cell.backgroundColor = self.convertHexToUIColor(hexColor: self.colorList[indexPath.row])
+        cell.backgroundColor = self.convertHexToUIColor(hexColor: self.colorList[(indexPath as NSIndexPath).row])
         
         // return cell
         return cell
@@ -97,10 +97,10 @@ class ColorPickerViewController: UIViewController, UICollectionViewDataSource, U
     
     
     // function - called when clicked on a collection view cell
-    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         
         // find clicked color value from colorList (it wil be in hex)
-        let clickedHexColor = self.colorList[indexPath.row]
+        let clickedHexColor = self.colorList[(indexPath as NSIndexPath).row]
         
         // convert hex color to UI Color
         let clickedUIColor = self.convertHexToUIColor(hexColor: clickedHexColor)
@@ -119,10 +119,10 @@ class ColorPickerViewController: UIViewController, UICollectionViewDataSource, U
     // MARK: - Utility functions
     
     // load colors from Colors.plist and save to colorList array.
-    private func loadColorList(){
+    fileprivate func loadColorList(){
     
         // create path for Colors.plist resource file.
-        let colorFilePath = NSBundle.mainBundle().pathForResource("Colors", ofType: "plist")
+        let colorFilePath = Bundle.main.path(forResource: "Colors", ofType: "plist")
 
         // save piist file array content to NSArray object
         let colorNSArray = NSArray(contentsOfFile: colorFilePath!)
@@ -133,30 +133,30 @@ class ColorPickerViewController: UIViewController, UICollectionViewDataSource, U
     
     
     // convert Hex string '#FF00FF' or 'FF00FF' to UIColor object
-    private func convertHexToUIColor(hexColor hexColor : String) -> UIColor {
+    fileprivate func convertHexToUIColor(hexColor : String) -> UIColor {
         
         // define character set (include whitespace, newline character etc.)
-        let characterSet = NSCharacterSet.whitespaceAndNewlineCharacterSet() as NSCharacterSet
+        let characterSet = CharacterSet.whitespacesAndNewlines as CharacterSet
         
         //trim unnecessary character set from string
-        var colorString : String = hexColor.stringByTrimmingCharactersInSet(characterSet)
+        var colorString : String = hexColor.trimmingCharacters(in: characterSet)
         
         // convert to uppercase
-        colorString = colorString.uppercaseString
+        colorString = colorString.uppercased()
         
         //if # found at start then remove it.
         if colorString.hasPrefix("#") {
-            colorString =  colorString.substringFromIndex(colorString.startIndex.advancedBy(1))
+            colorString =  colorString.substring(from: colorString.index(colorString.startIndex, offsetBy: 1))
         }
         
         // hex color must 6 chars. if invalid character count then return black color.
         if colorString.characters.count != 6 {
-            return UIColor.blackColor()
+            return UIColor.black
         }
         
         // split R,G,B component
         var rgbValue: UInt32 = 0
-        NSScanner(string:colorString).scanHexInt(&rgbValue)
+        Scanner(string:colorString).scanHexInt32(&rgbValue)
         let valueRed    = CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0
         let valueGreen  = CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0
         let valueBlue   = CGFloat(rgbValue & 0x0000FF) / 255.0
@@ -168,8 +168,8 @@ class ColorPickerViewController: UIViewController, UICollectionViewDataSource, U
     
     
     // close color picker view controller
-    private func closeColorPicker(){
-        self.dismissViewControllerAnimated(true, completion: nil)
+    fileprivate func closeColorPicker(){
+        self.dismiss(animated: true, completion: nil)
     }
 
 }
